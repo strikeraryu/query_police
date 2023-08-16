e1870cb2efcbb5d5924345bc7d8f6796cc47eccd
strikeraryu <striker.aryu56@gmail.com>
2023-08-16 15:42:37 +0530

    remove: custom absent value

--- lib/query_police/transform.rb
+++ lib/query_police/transform.rb
@@ -4,18 +4,9 @@
 module QueryPolice
   # This module define transformer methods for query police
   module Transform
-    def absent_value(column_rules)
-      case column_rules.dig("value_type")
-      when "array"
-        column_rules.dig("delimiter").present? ? "" : []
-      when "number"
-        0
-      else
-        "absent"
-      end
-    end
-
     def amount(value, column_rules)
+      return 0 if value.eql?("absent")
+
       column_rules.dig("value_type").eql?("number") ? value.to_f : value.size
     end
 
@@ -24,7 +15,7 @@ module QueryPolice
     end
 
     def value(value, column_rules)
-      value ||= absent_value(column_rules)
+      return "absent" if value.nil?
 
       if column_rules.dig("value_type").eql?("array") && column_rules.dig("delimiter").present?
         value = value.split(column_rules.dig("delimiter")).map(&:strip)
@@ -33,7 +24,7 @@ module QueryPolice
       value
     end
 
-    module_function :absent_value, :amount, :tag_rule, :value
+    module_function :amount, :tag_rule, :value
   end
 
   private_constant :Transform
