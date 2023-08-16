986cc49ac52f27ce41c6fd760b0c501148447977
strikeraryu <striker.aryu56@gmail.com>
2023-08-16 20:24:53 +0530

    fix: unsupported group by, order by

--- lib/query_police/explain.rb
+++ lib/query_police/explain.rb
@@ -59,6 +59,8 @@ module QueryPolice
 
       def parse_detailed_explain(explain_result)
         parsed_result = JSON.parse(explain_result&.first&.first || "{}").dig("query_block")
+        parsed_result = parsed_result.dig("ordering_operation") || parsed_result
+        parsed_result = parsed_result.dig("grouping_operation") || parsed_result
         return parsed_result.dig("nested_loop").map { |e| e.dig("table") } if parsed_result.key?("nested_loop")
 
         parsed_result.key?("table") ? [parsed_result.dig("table")] : []
